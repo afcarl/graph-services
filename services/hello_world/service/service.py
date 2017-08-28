77d4ddb63c3ade01e5c50a773149933402893aa1
Keiichiro Ono <keiono@gmail.com>
2017-08-28 14:47:57 -0700

    Simple example with Docker files added.

--- services/hello_world/service/service.py
+++ services/hello_world/service/service.py
@@ -1,17 +1,25 @@
 import networkx as nx
 import cxmate
+import logging
 
 
 class HelloService(cxmate.Service):
-
     def process(self, params, input_stream):
-        network = cxmate.Adapter.to_networkx(input_stream)
+        logging.warn(params)
 
-        # Dummy service here...
-        print('OK')
-        return cxmate.Adapter.from_networkx(network)
+        networks = cxmate.Adapter.to_networkx(input_stream)
+
+        for net in networks:
+            # Calculate some statistics of the graph
+            net.graph['label'] = 'out_net'
+            net.graph['name'] = params['name']
+            net.graph['density'] = nx.density(net)
+            net.graph['average_clustering'] = nx.average_clustering(net)
+
+        return cxmate.Adapter.from_networkx(networks)
 
 
 if __name__ == "__main__":
-  my_service = HelloService()
-  my_service.run()
+    my_service = HelloService()
+    logging.warn('Starting service...')
+    my_service.run()
