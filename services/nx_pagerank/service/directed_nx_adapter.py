087082bbd6ac8bf0d93069b405f5c75401ec69c0
a-simada <a-simada@ics.es.osaka-u.ac.jp>
2017-08-30 15:17:41 -0700

    add nx_pagerank service

---
+++ services/nx_pagerank/service/directed_nx_adapter.py
@@ -0,0 +1,54 @@
+import cxmate
+from cxmate.service import Adapter
+import networkx
+import logging
+
+
+class DiAdapter(cxmate.Adapter):
+
+    @staticmethod
+    def to_directed_networkx(ele_iter):
+        """
+        Creates a list of networkx objects by read network elements from ele_iter
+
+        :param ele_iter: A CX element generator
+        :returns: A list of networkx objects
+        """
+        networks = []
+        while ele_iter:
+            network, ele_iter = DiAdapter.read_directed_networkx(ele_iter)
+            networks.append(network)
+        return networks
+       
+    @staticmethod
+    def read_directed_networkx(ele_iter):
+        network = networkx.DiGraph()
+        attrs = []
+        edges = {}
+        for ele in ele_iter:
+            if not 'label' in network.graph:
+                network.graph['label'] = ele.label
+            if ele.label != network.graph['label']:
+                return network, itertools.chain([ele], ele_iter)
+            ele_type = ele.WhichOneof('element')
+            if ele_type == 'node':
+                node = ele.node
+                network.add_node(int(node.id), name=node.name)
+            elif ele_type == 'edge':
+                edge = ele.edge
+                src, tgt = int(edge.sourceId), int(edge.targetId)
+                edges[int(edge.id)] = (src, tgt)
+                network.add_edge(src, tgt, id=int(edge.id), interaction=edge.interaction)
+            elif ele_type == 'nodeAttribute':
+                attr = ele.nodeAttribute
+                network.add_node(attr.nodeId, **{attr.name: Adapter.parse_value(attr)})
+            elif ele_type == 'edgeAttribute':
+                attr = ele.edgeAttribute
+                attrs.append(attr)
+            elif ele_type == 'networkAttribute':
+                attr = ele.networkAttribute
+                network.graph[attr.name] = Adapter.parse_value(attr)
+            for attr in attrs:
+                source, target = edges[int(attr.edgeId)]
+                network[source][target][attr.name] = Adapter.parse_value(attr)
+        return network, None
---
+++ services/nx_pagerank/service/service.py
@@ -0,0 +1,35 @@
+import cxmate
+from cxmate.service import Adapter
+import networkx
+import logging
+from directed_nx_adapter import DiAdapter
+
+
+class MyService(cxmate.Service):
+
+    def process(self, params, input_stream):
+        for k, v in params.items():
+            if v == 'None':
+                params[k] = None
+
+        logging.warn(params)
+
+        if params['is_directed']:
+            # network is directed graph
+            network = DiAdapter.to_directed_networkx(input_stream)
+        else:
+            # network is non-directed graph
+            network = Adapter.to_networkx(input_stream)
+
+        del params['is_directed']
+
+        for net in network:
+            net.graph['label'] = 'Output'
+            pr = networkx.pagerank(net, **params)
+            networkx.set_node_attributes(net, 'pagerank', pr)
+
+        return Adapter.from_networkx(network)
+
+if __name__ == "__main__":
+    myService = MyService()
+    myService.run()
