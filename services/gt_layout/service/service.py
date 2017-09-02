2222e9c2da808bcd9364840908cd613e17003d31
Mitsuhiro ETO <frn.fnts@gmail.com>
2017-09-01 18:04:43 -0700

    Make the layout algorithm of gt_layout can be chosen

--- services/gt_layout/service/service.py
+++ services/gt_layout/service/service.py
@@ -12,30 +12,30 @@ from Adapter import Adapter
 
 
 class GtLayoutService(cxmate.Service):
-
-    def __prob(self, a, b):
-        if a == b:
-            return 0.999
-        else:
-            return 0.001
+    def __init__(self):
+        self.layouts = {
+            "sfdp_layout": {"function": gt.sfdp_layout, "parameter": []},
+            "fruchterman_reingold_layout": {"function": gt.fruchterman_reingold_layout, "parameter": []},
+            "arf_layout": {"function": gt.arf_layout, "parameter": []},
+            "radial_tree_layout": {"function": gt.radial_tree_layout, "parameter": ["root"]},
+            "planar_layout": {"function": gt.planar_layout, "parameter": []},
+            "random_layout": {"function": gt.random_layout, "parameter": []},
+            # "get_hierarchy_control_points": {"function": gt.get_hierarchy_control_points, "parameter": []},  # not supported
+        }
 
     def process(self, params, input_stream):
         logger.warn(params)
+        layout_name = params["layout-name"]
+        layout = self.layouts[layout_name]
 
-        # Toy example: generate random graph
-        # g, bm = gt.random_graph(1000, lambda: np.random.poisson(10), directed=False,
-        #                         model = "blockmodel",
-        #                         block_membership = lambda: np.random.randint(10),
-        #                         edge_probs = self.__prob)
-        g = gt.price_network(3000)
-        gl = [g]
-        # g = Adapter.to_graph_tool(input_stream)
+        parameter = {p: params[p] for p in layout["parameter"]}
+
+        g = Adapter.to_graph_tool(input_stream)
         if params["with-position"]:
-            pos = gt.sfdp_layout(g)
-            # pos = gt.sfdp_layout(g, groups=bm)
-            return Adapter.from_graph_tool(gl, pos)
+            pos = layout["function"](g[0], **parameter)
+            return Adapter.from_graph_tool(g, pos)
         else:
-            return Adapter.from_graph_tool(gl)
+            return Adapter.from_graph_tool(g)
 
 
 if __name__ == "__main__":
