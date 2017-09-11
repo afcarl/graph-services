3817eb3720cfaa77a20513a7cb369c1ce33a4540
a-simada <a-simada@ics.es.osaka-u.ac.jp>
2017-09-11 09:40:57 -0700

    add support for input parameters of dictionary type for nx_layout

--- services/nx_layout/service/service.py
+++ services/nx_layout/service/service.py
@@ -1,38 +1,66 @@
 import cxmate
-import networkx
 from networkx.drawing.nx_pydot import graphviz_layout
 from cxmate.service import NetworkElementBuilder
 import logging
 
+logging.basicConfig(level=logging.DEBUG)
 
-class MyService(cxmate.Service):
+# Label for CXmate output
+OUTPUT_LABEL = 'out_net'
+
+
+class NxLayoutService(cxmate.Service):
 
     def process(self, params, input_stream):
-        logging.warn(params)
+        """
+        CI service for creating node positions for the given network data using Graphviz.
+        """
 
-        network = cxmate.Adapter.to_networkx(input_stream)
+        # Replace string None to Python None data type
+        for k, v in params.items():
+            if v == str(None):
+                params[k] = None
 
-        nodedata_tmp = []        
-        for net in network:
-            net.graph['label'] = 'Output'
+        networks = cxmate.Adapter.to_networkx(input_stream)
+        nodedata_tmp = []
+
+        # For now, this service supports for single-graph only.
+        net = networks[0]
+
+        net.graph['label'] = OUTPUT_LABEL
+        if params['prog'] == 'twopi' and 'root' in net.graph.keys():
+            # When 'twopi' is selected in parameter 'prog', root node is used 'root' in NetworkAttributes
+            params['root'] = net.graph['root']
         for n, nodedata in net.nodes_iter(data=True):
+            # Prevent duplication of attribute 'name'.
             if 'name' in nodedata.keys():
                 nodedata_tmp.append(nodedata['name'])
                 del nodedata['name']
-        pos = graphviz_layout(net, prog=params['prog'], root=params['root'])
+        logging.debug(params)
+        pos = graphviz_layout(net, **params)
 
+        # Set removed attribute 'name' again.
         i = 0
         for n, nodedata in net.nodes_iter(data=True):
             nodedata['name'] = nodedata_tmp[i]
             i += 1
 
-        return self.outputStream(network, pos)
+        return self.outputStream([net], pos)
 
-    def outputStream(self, network, pos):
-        for i in cxmate.Adapter.from_networkx(network):
+    def outputStream(self, networks, pos):
+        """
+        Creates a CX element generator added cartesianCoordinate from a list of networkx objects.
+
+        :params networks: A list of networkX objects
+        :params pos: postions of nodes
+        :returns: A CX element generator
+        """
+
+        for i in cxmate.Adapter.from_networkx(networks):
             yield i
         for key in pos.keys():
-            builder = NetworkElementBuilder('Output')
+            # Make new element CartesianCoordinate
+            builder = NetworkElementBuilder(OUTPUT_LABEL)
             ele = builder.new_element()
             layout = ele.CartesianCoordinate
             layout.nodeId = key
@@ -42,5 +70,6 @@ class MyService(cxmate.Service):
 
 
 if __name__ == "__main__":
-    myService = MyService()
-    myService.run()
+    layout = NxLayoutService()
+    logging.info('Starting networkx layout service...')
+    layout.run()
