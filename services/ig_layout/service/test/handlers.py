0151c70b81698281dfb9e729629d817f4cf4a479
a-simada <a-simada@ics.es.osaka-u.ac.jp>
2017-09-13 11:04:28 -0700

    add simple unittest for ig_layout

---
+++ services/ig_layout/service/test/handlers.py
@@ -0,0 +1,167 @@
+import logging
+logging.basicConfig(level=logging.DEBUG)
+
+# Tag for default algorithm
+DEFAULT_HANDLER = 'default'
+
+# Attribute name for the communities
+COMMUNITY = 'community'
+
+
+class IgLayoutHandlers():
+    """
+    Provides actual community detection algorithms from pre-defined
+    list of igraph functions.
+
+    """
+
+    def __init__(self):
+
+        self.__handlers = {
+            'default': self.__circle,
+
+            'bipartite': self.__bipartite,
+            'circle': self.__circle,
+            'drl': self.__drl,
+            'fruchterman_reingold': self.__fruchterman_reingold,
+            'graphopt': self.__graphopt,
+            'grid': self.__grid,
+            'kamada_kawai': self.__kamada_kawai,
+            'lgl': self.__lgl,
+            'random': self.__random,
+            'reingold_tilford': self.__reingold_tilford,
+            'reingold_tilford_circular': self.__reingold_tilford_circular,
+            'star': self.__star
+        }
+
+    def get_handler(self, algorithm_name):
+
+        if algorithm_name in self.__handlers:
+            logging.debug("algorithm_name: {}".format(algorithm_name))
+            return self.__handlers[algorithm_name]
+        else:
+            logging.warn('Algorithm not available.  Using default one instead...')
+            return self.__handlers[DEFAULT_HANDLER]
+
+    def __bipartite(self, net, **params):
+        parameters_list = ['types', 'hgap', 'vgap', 'maxiter']
+        params = {i: params[i] for i in parameters_list}
+        params['types'] = net.vs[params['types']] if params['types'] is not None else None
+        params['maxiter'] = net.vs[params['maxiter']] if params['maxiter'] is not None else 100
+        logging.debug(params)
+        pos = net.layout_bipartite(**params)
+        return pos
+
+    def __circle(self, net, **params):
+        parameters_list = ['dim']
+        params = {i: params[i] for i in parameters_list}
+        logging.debug(params)
+        pos = net.layout_circle(**params)
+        return pos
+
+    def __drl(self, net, **params):
+        parameters_list = ['weights', 'fixed', 'seed', 'options', 'dim']
+        params = {i: params[i] for i in parameters_list}
+        params['weights'] = net.es[params['weights']] if params['weights'] is not None else None
+        params['fixed'] = net.vs[params['fixed']] if params['fixed'] is not None else None
+        # parameter 'seed' is not supported now.
+        params['seed'] = None
+        logging.debug(params)
+        pos = net.layout_drl(**params)
+        return pos
+
+    def __fruchterman_reingold(self, net, **params):
+        parameters_list = ['weights', 'maxiter', 'maxdelta', 'area', 'coolexp', 'repulserad', 'minx', 'maxx', 'miny', 'maxy', 'minz', 'maxz', 'seed', 'dim']
+        params = {i: params[i] for i in parameters_list}
+        params['weights'] = net.es[params['weights']] if params['weights'] is not None else None
+        params['maxiter'] = net.vs[params['maxiter']] if params['maxiter'] is not None else 500
+        params['coolexp'] = net.vs[params['coolexp']] if params['coolexp'] is not None else 1.5
+        vs_params = ['minx', 'maxx', 'miny', 'maxy', 'minz', 'maxz']
+        for p in vs_params:
+            params[p] = net.vs[params[p]] if params[p] is not None else None
+        # parameter 'seed' is not supported now.
+        params['seed'] = None
+        logging.debug(params)
+        pos = net.layout_fruchterman_reingold(**params)
+        return pos
+
+    def __graphopt(self, net, **params):
+        parameters_list = ['niter', 'node_charge', 'node_mass', 'spring_length', 'spring_constant', 'max_sa_movement', 'seed']
+        params = {i: params[i] for i in parameters_list}
+        # parameter 'seed' is not supported now.
+        params['seed'] = None
+        logging.debug(params)
+        pos = net.layout_graphopt(**params)
+        return pos
+
+    def __grid(self, net, **params):
+        parameters_list = ['width', 'height', 'dim']
+        params = {i: params[i] for i in parameters_list}
+        logging.debug(params)
+        pos = net.layout_grid(**params)
+        return pos
+
+    def __kamada_kawai(self, net, **params):
+        parameters_list = ['maxiter', 'sigma', 'initemp', 'coolexp', 'kkconst', 'minx', 'maxx', 'miny', 'maxy', 'minz', 'maxz', 'seed', 'dim']
+        params = {i: params[i] for i in parameters_list}
+        params['maxiter'] = net.vs[params['maxiter']] if params['maxiter'] is not None else 1000
+        params['coolexp'] = net.vs[params['coolexp']] if params['coolexp'] is not None else 0.99
+        vs_params = ['minx', 'maxx', 'miny', 'maxy', 'minz', 'maxz']
+        for p in vs_params:
+            params[p] = net.es[params[p]] if params[p] is not None else None
+        # parameter 'seed' is not supported now.
+        params['seed'] = None
+        logging.debug(params)
+        pos = net.layout_kamada_kawai(**params)
+        return pos
+
+    def __lgl(self, net, **params):
+        parameters_list = ['maxiter', 'maxdelta', 'area', 'coolexp', 'repulserad', 'cellsize', 'root']
+        params = {i: params[i] for i in parameters_list}
+        params['maxiter'] = net.vs[params['maxiter']] if params['maxiter'] is not None else 150
+        params['coolexp'] = net.vs[params['coolexp']] if params['coolexp'] is not None else 1.5
+        # parameter 'root' is not supported now.
+        params['root'] = None
+        logging.debug(params)
+        pos = net.layout_lgl(**params)
+        return pos
+
+    def __random(self, net, **params):
+        parameters_list = ['dim']
+        params = {i: params[i] for i in parameters_list}
+        logging.debug(params)
+        pos = net.layout_random(**params)
+        return pos
+
+    def __reingold_tilford(self, net, **params):
+        # TODO: Fix a bug
+        parameters_list = ['mode', 'root', 'rootlevel']
+        params = {i: params[i] for i in parameters_list}
+        # parameter 'root' and 'rootlevel' are not supported now.
+        params['root'] = None
+        params['rootlevel'] = None
+        logging.debug(params)
+        pos = net.layout_reingold_tilford(**params)
+        return pos
+
+    def __reingold_tilford_circular(self, net, **params):
+        # TODO: Fix a bug
+        parameters_list = ['mode', 'root', 'rootlevel']
+        params = {i: params[i] for i in parameters_list}
+        # parameter 'root' and 'rootlevel' are not supported now.
+        params['root'] = None
+        params['rootlevel'] = None
+        logging.debug(params)
+        pos = net.layout_reingold_tilford_circular(**params)
+        return pos
+
+    def __star(self, net, **params):
+        parameters_list = ['center', 'order']
+        params = {i: params[i] for i in parameters_list}
+        params['order'] = net.es[params['order']] if params['order'] is not None else None
+        # parameter 'center' is not supported now.
+        params['center'] = None
+        logging.debug(params)
+        pos = net.layout_star(**params)
+        return pos
+
---
+++ services/ig_layout/service/test/myservice.py
@@ -0,0 +1,66 @@
+import cxmate
+import logging
+import igraph as ig
+from Adapter import NetworkElementBuilder, IgraphAdapter
+from handlers import IgLayoutHandlers
+
+logger = logging.getLogger('igraph_service')
+logging.basicConfig(level=logging.DEBUG)
+
+# Label for CXmate output
+OUTPUT_LABEL = 'out_net'
+
+# Community detection algorithm name
+ALGORITHM_TYPE = 'layout'
+
+
+class IgLayoutService(cxmate.Service):
+
+    def __init__(self):
+        self.__handlers = IgLayoutHandlers()
+
+    def process(self, params, input_stream):
+        algorithm_type = params[ALGORITHM_TYPE]
+        del params[ALGORITHM_TYPE]
+
+        for k, v in params.items():
+            if v == str(None):
+                params[k] = None
+
+        ig_networks = IgraphAdapter.to_igraph(input_stream)
+        pos_dict = {}
+
+        for net in ig_networks:
+            net['label'] = OUTPUT_LABEL
+
+            # Get the layout function by name of the algorithm
+            handler = self.__handlers.get_handler(algorithm_type)
+
+            # Call the function to calculate layout
+            pos = handler(net, **params)
+
+            for i, vertex in enumerate(net.vs):
+                pos_dict[vertex['nodeId']] = pos[i]
+
+        return self.outputStream(ig_networks, pos_dict)
+
+    def outputStream(self, networks, pos):
+        for i in IgraphAdapter.from_igraph(networks):
+            yield i
+        for k, v in pos.items():
+            builder = NetworkElementBuilder('out_net')
+            ele = builder.new_element()
+            layout = ele.CartesianCoordinate
+            layout.nodeId = k
+            layout.x = v[0]
+            layout.y = v[1]
+            if len(v) == 3:
+                # dim = 3
+                layout.z = v[2]
+            yield ele
+
+if __name__ == "__main__":
+
+    analyzer = IgLayoutService()
+    logging.warn('Starting igraph analysis service...')
+    analyzer.run()
